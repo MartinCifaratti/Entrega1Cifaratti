b82c3f0ec1c8fc16cf2286aec2e2c94115cc66f7
Martin Cifaratti <martincifaratti@hotmail.com>
2022-06-17 21:33:19 -0300

    Tercera versión

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -3,14 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name = "Inicio"),
-    path('curso', views.cursos, name = "Cursos"),
+    path('cursos', views.cursos, name = "Cursos"),
     path('profesores', views.profesores, name = "Profesores"),
     path('estudiantes', views.estudiantes, name = "Estudiantes"),
     path('entregables', views.entregables, name = "Entregables"),
-    path('cursoFormulario', views.cursoFormulario, name = "CursoFormularios"),
-    path('alta_curso', views.cursoFormulario),
-    path('buscar_curso', views.buscar_curso),
+    #path('cursoFormulario', views.cursoFormulario, name = "CursoFormularios"),
+    path('alta_curso', views.cursoFormulario, name = "altacurso"),
+    path('buscar_curso', views.buscar_curso, name = "buscarcurso"),
     path('buscar', views.buscar),
+    path('formularioprofesor', views.formularioprofesor),
 ]
 
 
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from AppCoder.models import *
 from django.template import loader
+from AppCoder.forms import *
 # Create your views here.
 def inicio(request):
     return render(request, 'AppCoder/inicio.html')
@@ -9,9 +10,9 @@ def inicio(request):
 def cursos(request):
     cursos = Curso.objects.all()
     dicc = {"cursos": cursos}
-    plantilla = loader.get_template("cursos.html")
+    plantilla = loader.get_template("Appcoder/cursos.html")
     documento = plantilla.render(dicc)
-    return HTTPResponse( documento)
+    return HttpResponse( documento)
 
 def profesores(request):
     return render(request, 'AppCoder/profesores.html')
@@ -22,23 +23,23 @@ def estudiantes(request):
 def entregables(request):
     return render(request, 'AppCoder/entregables.html')
 
-def alta_curso(request, nombre):
-    curso = Curso(nombre= nombre , camada= 287318)
-    curso.save()
-    texto = f"Se guardo en la BD el Curso: {curso.nombre} Camada: {curso.camada}"
-    return HTTPResponse (texto)
+#def alta_curso(request, nombre):
+#    curso = Curso(nombre= nombre , camada= 287318)
+#    curso.save()
+#    texto = f"Se guardo en la BD el Curso: {curso.nombre} Camada: {curso.camada}"
+#    return HttpResponse (texto)
 
 def cursoFormulario(request):
-    if request .method == "POST":
+    if request.method == 'POST':
         mi_formulario = CursoFormulario(request.POST)
         if mi_formulario.is_valid():
             datos = mi_formulario.cleaned_data
         
-        curso= Curso(nombre=datos['nombre'], camada= datos['camada'])
-        curso.save()
+            curso= Curso(nombre=datos['nombre'], camada= datos['camada'])
+            curso.save()
         
-        return render(request, "AppCoder/cursoFormulario.html")
-    return render(request, "AppCoder/cursoFormulario.html")
+            return render(request, "AppCoder/cursoFormulario.html")
+    return render (request, "Appcoder/cursoFormulario.html")
 
 def buscar_curso(request):
     return render(request, "AppCoder/buscar_curso.html")
@@ -49,4 +50,17 @@ def buscar(request):
         cursos = Curso.objects.filter(nombre__icontains = nombre)
         return render( request, "AppCoder/resultado_busqueda.html", {"cursos": cursos} )
     else:
-        return HTTPResponse("Campo vacio")
+        return HttpResponse("Campo vacio")
+
+def formularioprofesor(request):
+    if request .method == "POST":
+        mi_formulario = formularioprofesor(request.POST)
+        if mi_formulario.is_valid():
+            datos = mi_formulario.cleaned_data
+        
+        profesor= Profesor(nombre=datos['nombre'], apellido= datos['apellido'], email= datos['email'], profesion= datos['profesion'])
+        profesor.save()
+        
+        return render(request, "AppCoder/formularioprofesor.html")
+    else:
+        return HttpResponse("Campo vacio")
